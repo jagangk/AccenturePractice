a58d19c1d96c711b0b27287ff336142c01fae777
jagangk <jaganvijayakumar2003@gmail.com>
2024-04-30 20:27:53 +0530

    decimal to bin toggle bits and convert to decimal again

--- AccenturePractice.py
+++ AccenturePractice.py
@@ -1,12 +1,4 @@
-def empty_packets_checker(arr):
-    j = 0
-    for i in range(len(arr)):
-        if arr[i] != 0:
-            arr[j] = arr[i]
-            j = j + 1
-    for k in range(j, len(arr)):
-        arr[k] = 0
-    return arr
-arr = [8,5,0,0,0,0]
-result = empty_packets_checker(arr)
-print(result)
+N = int(input("plz enter a number: "))
+binary_num = bin(N)[2:]
+toggled_bit = ''.join('1' if bit == '0' else '0' for bit in binary_num)
+print(int(toggled_bit, 2))
