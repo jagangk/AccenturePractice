5c8193b0fb23d2d6f66f47fd6862aa9489684f12
jagangk <jaganvijayakumar2003@gmail.com>
2024-04-30 19:59:10 +0530

    empty packet checker program

--- AccenturePractice.py
+++ AccenturePractice.py
@@ -1,1 +1,12 @@
-print('hello world')
+def empty_packets_checker(arr):
+    j = 0
+    for i in range(len(arr)):
+        if arr[i] != 0:
+            arr[j] = arr[i]
+            j = j + 1
+    for k in range(j, len(arr)):
+        arr[k] = 0
+    return arr
+arr = [8,5,0,0,0,0]
+result = empty_packets_checker(arr)
+print(result)
